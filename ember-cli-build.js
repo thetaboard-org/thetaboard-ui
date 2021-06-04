d17b16866d60871637ad4ff8765e86497f516939
maurinlenglart <maurin.lenglart@gmail.com>
2021-06-03 18:41:26 -0700

    compile and keep original files as well

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
-
+    gzip:{
+      keepUncompressed: true
+    },
     // Add options here
     fingerprint: {
       enabled: false
