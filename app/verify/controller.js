73e228b54981cfbc0ae269548b4e915f9ac176b6
Adrien Vinches <{ID}+{username}@users.noreply.github.com>
2021-06-10 13:34:46 -0700

    redirect user after email validation

--- app/verify/controller.js
+++ app/verify/controller.js
@@ -1,7 +1,24 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class VerifyController extends Controller {
+  @service session;
+  @service currentUser;
+
+  get isUserAuthenticated() {
+    if (
+      this.session.isAuthenticated &&
+      this.currentUser.user &&
+      this.model.data.id
+    ) {
+      if (this.currentUser.user.id == this.model.data.id) {
+        return true;
+      }
+    }
+    return false;
+  }
+
   @action
   setQueryParam(walletAddress) {
     this.transitionToRoute({ queryParams: { wa: walletAddress } });
