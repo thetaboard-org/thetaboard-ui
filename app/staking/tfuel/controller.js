7fe2a8f7452e784fa44311ac198b604da4ad6071
Adrien Vinches <{ID}+{username}@users.noreply.github.com>
2021-07-08 16:24:48 -0600

    sort EEN from emtpy to full

--- app/staking/tfuel/controller.js
+++ app/staking/tfuel/controller.js
@@ -2,6 +2,7 @@ import Controller from '@ember/controller';
 import {action} from '@ember/object';
 import {inject as service} from '@ember/service';
 import { tracked } from '@glimmer/tracking';
+import { sort } from '@ember/object/computed';
 
 export default class TfuelStakingController extends Controller {
   @tracked modalSummary;
@@ -11,6 +12,17 @@ export default class TfuelStakingController extends Controller {
   get edgeNodeList() {
     return this.model.publicEdgeNodes;
   }
+
+  @sort('edgeNodeList', function (a, b) {
+    if (a['stakeAmount'] > b['stakeAmount']) {
+      return 1;
+    } else if (a['stakeAmount'] < b['stakeAmount']) {
+      return -1;
+    }
+
+    return 0;
+  })
+  edgeNodeListSorted;
 
   @action
   setupModalSummary(summary) {
