ab903309e59a7fc4664757e0e59b1a241f6aea1e
Adrien Vinches <{ID}+{username}@users.noreply.github.com>
2021-07-30 01:23:48 -0600

    transaction list name

--- app/models/transaction-history.js
+++ app/models/transaction-history.js
@@ -1,6 +1,10 @@
 import Model, { attr } from '@ember-data/model';
+import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class TransactionHistoryModel extends Model {
+  @service intl
+
   @attr('string') walletAddress;
   @attr('string') blockHash
   @attr('number') blockHeight
@@ -28,4 +32,28 @@ export default class TransactionHistoryModel extends Model {
   //transaction list component
   @attr('string') inOrOut
   @attr() values
+
+  @computed('type')
+  get typeName() {
+    if (this.type == 0) {
+      return this.intl.t('global.reward');
+    } else if (this.type == 2) {
+      return this.intl.t('global.transfer');
+    } else if (this.type == 3) {
+      return this.intl.t('global.reserve_fund');
+    } else if (this.type == 5) {
+      return this.intl.t('global.service_payment');
+    } else if (this.type == 6) {
+      return this.intl.t('global.split_contract');
+    } else if (this.type == 7) {
+      return this.intl.t('global.smart_contract');
+    } else if (this.type == 9) {
+      return this.intl.t('global.withdraw_stake');
+    } else if (this.type == 10) {
+      return this.intl.t('global.deposit_stake');
+    } else if (this.type == 11) {
+      return this.intl.t('global.stake_reward_distribution');
+    }
+    return '';
+  }
 }
