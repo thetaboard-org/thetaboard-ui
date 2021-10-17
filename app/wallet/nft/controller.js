ae3d36d2d2b07ffcd01a5baccfa4c154aff89485
maurinlenglart <maurin.lenglart@gmail.com>
2021-10-17 15:50:09 -0700

    cleaner code + language

--- app/wallet/nft/controller.js
+++ app/wallet/nft/controller.js
@@ -23,30 +23,23 @@ export default class NFTController extends Controller {
   @action
   async metamask() {
     try {
-      if (ethereum && ethereum.isConnected()) {
-        if (parseInt(ethereum.chainId) !== 361) {
-          window.web3 = new Web3(window.web3.currentProvider);
-          const sell_contract = new window.web3.eth.Contract(this.abi.ThetaboardDirectSell, "0xfbea9043f909b37a83ee9158b6698df8bec98553");
-          const accounts = await ethereum.request({method: 'eth_requestAccounts'});
-          const account = accounts[0];
-          const nft_sell = await sell_contract.methods.purchaseToken("0x7500CBde64B1bf956351Aa4ea2fa4eE1467a3428").send({
-            value: window.web3.utils.toWei("20"),
-            from: account
-          });
-          this.utils.successNotify(this.intl.t('notif.success_nft'));
-        } else {
-          this.utils.errorNotify(this.intl.t('notif.not_theta_blockchain'));
-        }
+      if (!ethereum || !ethereum.isConnected()) {
+        return this.utils.errorNotify(this.intl.t('notif.no_metamask'));
+      } else if (parseInt(ethereum.chainId) !== 361) {
+        return this.utils.errorNotify(this.intl.t('notif.not_theta_blockchain'));
       } else {
-        this.utils.errorNotify(this.intl.t('notif.no_metamask'));
+        window.web3 = new Web3(window.web3.currentProvider);
+        const sell_contract = new window.web3.eth.Contract(this.abi.ThetaboardDirectSell, "0xfbea9043f909b37a83ee9158b6698df8bec98553");
+        const accounts = await ethereum.request({method: 'eth_requestAccounts'});
+        const account = accounts[0];
+        const nft_sell = await sell_contract.methods.purchaseToken("0x7500CBde64B1bf956351Aa4ea2fa4eE1467a3428").send({
+          value: window.web3.utils.toWei("20"),
+          from: account
+        });
+        return this.utils.successNotify(this.intl.t('notif.success_nft'));
       }
     } catch (e) {
-      this.utils.errorNotify(e.message);
+      return this.utils.errorNotify(e.message);
     }
-
-    /* if it is not a theta blockchain*/
-
-
-    debugger
   }
 }
