8e7b910fc2a21ccb3f1d94bac00dc8bb53f07563
maurin.lenglart <momohuri>
2021-12-08 10:12:48 -0800

    transfert nft

---
+++ app/components/wallet/nft-action.js
@@ -0,0 +1,56 @@
+import Component from "@glimmer/component";
+import {inject as service} from '@ember/service';
+import {action} from '@ember/object';
+import {tracked} from "@glimmer/tracking";
+
+export default class NftActionComponent extends Component {
+  @service utils;
+  @service intl;
+  @service('abi') abi;
+
+  @tracked transfer_initiated = false;
+  @tracked destinationAddr;
+
+  get nft() {
+    return this.args.nft;
+  }
+
+  async metamask_connect() {
+    if (typeof ethereum === 'undefined' || !ethereum.isConnected()) {
+      return this.utils.errorNotify(this.intl.t('notif.no_metamask'));
+    } else if (parseInt(ethereum.chainId) !== 361) {
+      return this.utils.errorNotify(this.intl.t('notif.not_theta_blockchain'));
+    } else {
+      window.web3 = new Web3(window.web3.currentProvider);
+      const accounts = await ethereum.request({method: 'eth_requestAccounts'});
+      return accounts[0];
+    }
+  }
+
+  @action
+  async init_transfer() {
+    const account = await this.metamask_connect();
+    const nft_contract = new window.web3.eth.Contract(this.abi.ThetaboardNFT, this.nft.contract_addr);
+    const token_owner = await nft_contract.methods.ownerOf(this.nft.original_token_id).call();
+    if (token_owner.toLowerCase() !== account) {
+      return this.utils.errorNotify("The account connected on metamask need to be the owner of the nft");
+    }
+    this.transfer_initiated = true;
+  }
+
+
+  @action
+  async transfer(nft) {
+    try {
+      const account = await this.metamask_connect();
+      debugger
+      const nft_contract = new window.web3.eth.Contract(this.abi.ThetaboardNFT, this.nft.contract_addr);
+      await nft_contract.methods.transferFrom(account, this.destinationAddr, this.nft.original_token_id).send({
+        from: account
+      });
+      return this.utils.successNotify(this.intl.t('notif.success_nft'));
+    } catch (e) {
+      return this.utils.errorNotify(e.message);
+    }
+  }
+}
--- app/wallet/nft/controller.js
+++ app/wallet/nft/controller.js
@@ -20,24 +20,28 @@ export default class NFTController extends Controller {
     );
   }
 
+  async metamask_connect() {
+    if (typeof ethereum === 'undefined' || !ethereum.isConnected()) {
+      return this.utils.errorNotify(this.intl.t('notif.no_metamask'));
+    } else if (parseInt(ethereum.chainId) !== 361) {
+      return this.utils.errorNotify(this.intl.t('notif.not_theta_blockchain'));
+    } else {
+      window.web3 = new Web3(window.web3.currentProvider);
+      const accounts = await ethereum.request({method: 'eth_requestAccounts'});
+      return accounts[0];
+    }
+  }
+
   @action
   async metamask() {
     try {
-      if (typeof ethereum === 'undefined' || !ethereum.isConnected()) {
-        return this.utils.errorNotify(this.intl.t('notif.no_metamask'));
-      } else if (parseInt(ethereum.chainId) !== 361) {
-        return this.utils.errorNotify(this.intl.t('notif.not_theta_blockchain'));
-      } else {
-        window.web3 = new Web3(window.web3.currentProvider);
-        const sell_contract = new window.web3.eth.Contract(this.abi.ThetaboardDirectSell, "0xfbea9043f909b37a83ee9158b6698df8bec98553");
-        const accounts = await ethereum.request({method: 'eth_requestAccounts'});
-        const account = accounts[0];
-        const nft_sell = await sell_contract.methods.purchaseToken("0x7500CBde64B1bf956351Aa4ea2fa4eE1467a3428").send({
-          value: window.web3.utils.toWei("20"),
-          from: account
-        });
-        return this.utils.successNotify(this.intl.t('notif.success_nft'));
-      }
+      const account = await this.metamask_connect();
+      const sell_contract = new window.web3.eth.Contract(this.abi.ThetaboardDirectSell, "0xfbea9043f909b37a83ee9158b6698df8bec98553");
+      const nft_sell = await sell_contract.methods.purchaseToken("0x7500CBde64B1bf956351Aa4ea2fa4eE1467a3428").send({
+        value: window.web3.utils.toWei("20"),
+        from: account
+      });
+      return this.utils.successNotify(this.intl.t('notif.success_nft'));
     } catch (e) {
       return this.utils.errorNotify(e.message);
     }
