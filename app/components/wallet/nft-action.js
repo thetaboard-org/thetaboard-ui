0db3ce6de4910fc103ded64560ea5448d3aa97d0
maurin.lenglart <momohuri>
2021-12-08 16:28:26 -0800

    transfer nfr

--- app/components/wallet/nft-action.js
+++ app/components/wallet/nft-action.js
@@ -15,6 +15,32 @@ export default class NftActionComponent extends Component {
     return this.args.nft;
   }
 
+  get enableTooltip(){
+    $('[data-toggle="tooltip"]').tooltip();
+    return;
+  }
+
+  get metamaskAvailable() {
+    const checkMetamask = async () => {
+      if (typeof ethereum === 'undefined' || !ethereum.isConnected()) {
+        return this.intl.t('notif.no_metamask');
+      } else if (parseInt(ethereum.chainId) !== 361) {
+        return this.intl.t('notif.not_theta_blockchain');
+      } else {
+        window.web3 = new Web3(window.web3.currentProvider);
+        const account = await this.metamask_connect();
+        const nft_contract = new window.web3.eth.Contract(this.abi.ThetaboardNFT, this.nft.contract_addr);
+        const token_owner = await nft_contract.methods.ownerOf(this.nft.original_token_id).call();
+        if (token_owner.toLowerCase() !== account) {
+          return "The account connected on metamask need to be the owner of the nft";
+        }
+        return true;
+      }
+    }
+
+    return checkMetamask();
+  }
+
   async metamask_connect() {
     if (typeof ethereum === 'undefined' || !ethereum.isConnected()) {
       return this.utils.errorNotify(this.intl.t('notif.no_metamask'));
@@ -29,21 +55,14 @@ export default class NftActionComponent extends Component {
 
   @action
   async init_transfer() {
-    const account = await this.metamask_connect();
-    const nft_contract = new window.web3.eth.Contract(this.abi.ThetaboardNFT, this.nft.contract_addr);
-    const token_owner = await nft_contract.methods.ownerOf(this.nft.original_token_id).call();
-    if (token_owner.toLowerCase() !== account) {
-      return this.utils.errorNotify("The account connected on metamask need to be the owner of the nft");
-    }
     this.transfer_initiated = true;
   }
 
-
   @action
-  async transfer(nft) {
+  async transfer(event) {
+    event.preventDefault();
     try {
       const account = await this.metamask_connect();
-      debugger
       const nft_contract = new window.web3.eth.Contract(this.abi.ThetaboardNFT, this.nft.contract_addr);
       await nft_contract.methods.transferFrom(account, this.destinationAddr, this.nft.original_token_id).send({
         from: account
