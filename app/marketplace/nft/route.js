b5f49cb432c2d2615391b9a4435d85de3fcc59cf
maurin.lenglart <momohuri>
2022-01-03 10:58:34 -0800

    scroll up

--- app/marketplace/nft/route.js
+++ app/marketplace/nft/route.js
@@ -1,8 +1,16 @@
 import Route from '@ember/routing/route';
 
 export default class NftRoute extends Route {
+
+  activate() {
+    this._super(...arguments);
+    // scroll to the top of the page
+    window.scrollTo(0, 0);
+  }
+
+
   async model(params) {
     const nft = await this.store.findRecord('nft', params.nftId);
-    return { nft: nft };
+    return {nft: nft};
   }
 }
--- app/wallet/nft-info/route.js
+++ app/wallet/nft-info/route.js
@@ -1,9 +1,16 @@
 import Route from '@ember/routing/route';
 
 export default class NftInfoRoute extends Route {
+
+  activate() {
+    this._super(...arguments);
+    // scroll to the top of the page
+    window.scrollTo(0, 0);
+  }
+
   async model(params) {
     const fetched = await fetch(`/explorer/wallet-info/${params.contractAddr}/${params.tokenId}`);
     const nftInfo = await fetched.json();
-    return { nft: nftInfo };
+    return {nft: nftInfo};
   }
 }
