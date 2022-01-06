ef18bdf76faedca703565225e860a483109a531b
maurin.lenglart <momohuri>
2022-01-06 15:22:02 -0800

    fix nft assets + video

--- app/wallet/nft-info/route.js
+++ app/wallet/nft-info/route.js
@@ -11,6 +11,13 @@ export default class NftInfoRoute extends Route {
   async model(params) {
     const fetched = await fetch(`/explorer/wallet-info/${params.contractAddr}/${params.tokenId}`);
     const nftInfo = await fetched.json();
+    // Some hack because we don't have an ember object but just a raw NFT
+    if(nftInfo.properties.assets.length !== 0 && nftInfo.properties.assets[0].type === "video"){
+      nftInfo.isFirstAssetVideo = true;
+      const asset = nftInfo.properties.assets.shift();
+      nftInfo.firstAsset = asset.asset;
+    }
+
     return {nft: nftInfo};
   }
 }
