0fd0d492e728f08c531f1a1fbb40909bb70c5b10
maurin.lenglart <momohuri>
2022-01-13 14:32:35 -0800

    markplace approval

--- app/components/wallet/nft-action.js
+++ app/components/wallet/nft-action.js
@@ -15,30 +15,53 @@ export default class NftActionComponent extends Component {
     return this.args.nft;
   }
 
-  get enableTooltip(){
+  get enableTooltip() {
     $('[data-toggle="tooltip"]').tooltip();
     return;
   }
 
   get metamaskAvailable() {
+    // return 0 if metamask is not installed
+    // return 1 if not theta blockchain
+    // return 2 if metamask is installed but not linked to thetaboard.io
+    // return 3 if metamask is installed and linked but account is not the same as the NFT
+    // return 4 if everything is good
+
     const checkMetamask = async () => {
       if (typeof ethereum === 'undefined' || !ethereum.isConnected()) {
-        return this.intl.t('notif.no_metamask');
+        return 0;
       } else if (parseInt(ethereum.chainId) !== 361) {
-        return this.intl.t('notif.not_theta_blockchain');
+        return 1;
       } else {
         window.web3 = new Web3(window.web3.currentProvider);
-        const account = await this.metamask_connect();
+        const accounts = await window.web3.eth.getAccounts();
+        if (accounts.length === 0) {
+          return 2;
+        }
         const nft_contract = new window.web3.eth.Contract(this.abi.ThetaboardNFT, this.nft.contract_addr);
         const token_owner = await nft_contract.methods.ownerOf(this.nft.original_token_id).call();
-        if (token_owner.toLowerCase() !== account) {
-          return "The account connected on metamask need to be the owner of the nft";
+        if (token_owner !== accounts[0]) {
+          return 3;
         }
-        return true;
+        return 4;
       }
     }
-
     return checkMetamask();
+  }
+
+  get marketPlaceStatus() {
+    // return 0 not approved
+    // return 1 if approved
+    // return 2 if on sale
+
+    const checkIfApproved = async () => {
+      const account = await this.metamask_connect();
+      const nft_contract = new window.web3.eth.Contract(this.abi.ThetaboardNFT, this.nft.contract_addr);
+      const approved = await nft_contract.methods.getApproved(this.nft.original_token_id).call();
+      debugger
+      return approved === this.abi.ThetaboardMarketplaceAddr;
+    }
+    return checkIfApproved();
   }
 
   async metamask_connect() {
@@ -67,9 +90,38 @@ export default class NftActionComponent extends Component {
       await nft_contract.methods.transferFrom(account, this.destinationAddr, this.nft.original_token_id).send({
         from: account
       });
+      return this.utils.successNotify("Successfully approved for sell");
+    } catch (e) {
+      return this.utils.errorNotify(e.message);
+    }
+  }
+
+  @action
+  async approve_for_sell() {
+    try {
+      const account = await this.metamask_connect();
+      const nft_contract = new window.web3.eth.Contract(this.abi.ThetaboardNFT, this.nft.contract_addr);
+      await nft_contract.methods.approve(this.abi.ThetaboardMarketplaceAddr, this.nft.original_token_id).send({
+        from: account
+      });
       return this.utils.successNotify(this.intl.t('notif.success_nft'));
     } catch (e) {
       return this.utils.errorNotify(e.message);
     }
+  }
+
+  @action
+  async sell_nft(event){
+    event.preventDefault();
+    try{
+      const account = await this.metamask_connect();
+      const market = new window.web3.eth.Contract(this.abi.ThetaboardMarketplace, this.abi.ThetaboardMarketplaceAddr);
+      await market.methods.createMarketItem(this.nft.contract_addr, 2, this.sellPrice, "ThetaboardUser").send({
+        from: account
+      });
+    }catch (e) {
+      return this.utils.errorNotify(e.message);
+    }
+
   }
 }
