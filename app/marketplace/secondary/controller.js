55d4b1c307e97111d6d51dec44fd3c31a4a53002
maurin.lenglart <momohuri>
2022-02-22 14:02:49 -0800

    marketplace search working

--- app/marketplace/secondary/controller.js
+++ app/marketplace/secondary/controller.js
@@ -1,9 +1,47 @@
 import Controller from '@ember/controller';
+import { debounce } from '@ember/runloop';
+import {action} from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+
+
+
 
 export default class SecondaryController extends Controller {
+  @tracked search = '';
+  @tracked selectedArtists = [];
 
   get sellingNFTs(){
     return this.model.marketplaceInfo.sellingNFTs;
+  }
+
+  get artists(){
+    return this.model.marketplaceInfo.artists;
+  }
+
+  async searchMarketplaceFetch (){
+    if(this.search || this.selectedArtists.length !== 0){
+      const artistIds = this.selectedArtists.map((x)=>x.id).join(',')
+      const marketplaceInfoFetch = await fetch(`/api/marketplace/search?search=${this.search}&artists=${artistIds}`);
+      const marketplaceInfo = await marketplaceInfoFetch.json();
+      this.model = {marketplaceInfo: marketplaceInfo};
+    } else {
+      const marketplaceInfoFetch = await fetch(`/api/marketplace`);
+      const marketplaceInfo = await marketplaceInfoFetch.json();
+      this.model = {marketplaceInfo: marketplaceInfo};
+    }
+
+
+  }
+
+  @action
+  searchMarketplace(){
+    debounce(this, this.searchMarketplaceFetch, 500);
+  }
+
+  @action
+  changeArtist(artist){
+    this.selectedArtists = artist
+    debounce(this, this.searchMarketplaceFetch, 500);
   }
 
 }
