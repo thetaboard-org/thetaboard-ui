512948869375a608e87f58b3ae1fee6e5497e3af
maurin.lenglart <momohuri>
2022-02-25 10:53:41 -0800

    Drops added to search ui

--- app/marketplace/secondary/controller.js
+++ app/marketplace/secondary/controller.js
@@ -1,46 +1,62 @@
 import Controller from '@ember/controller';
-import { debounce } from '@ember/runloop';
-import {action} from '@ember/object';
-import { tracked } from '@glimmer/tracking';
-
-
+import {debounce} from '@ember/runloop';
+import {action, computed, set} from '@ember/object';
+import {tracked} from '@glimmer/tracking';
 
 
 export default class SecondaryController extends Controller {
   @tracked search = '';
   @tracked selectedArtists = [];
+  @tracked selectedDrops = [];
 
-  get sellingNFTs(){
+  @computed("model.marketplaceInfo")
+  get sellingNFTs() {
     return this.model.marketplaceInfo.sellingNFTs;
   }
 
-  get artists(){
-    return this.model.marketplaceInfo.artists;
+  get artists() {
+    return this.model.facets.artists;
   }
 
-  async searchMarketplaceFetch (){
-    if(this.search || this.selectedArtists.length !== 0){
-      const artistIds = this.selectedArtists.map((x)=>x.id).join(',')
-      const marketplaceInfoFetch = await fetch(`/api/marketplace/search?search=${this.search}&artists=${artistIds}`);
-      const marketplaceInfo = await marketplaceInfoFetch.json();
-      this.model = {marketplaceInfo: marketplaceInfo};
+  get drops() {
+    return this.model.facets.drops;
+  }
+
+  get categories() {
+    return this.model.facets.categories;
+  }
+
+  get priceRange() {
+    debugger
+  }
+
+  async searchMarketplaceFetch() {
+    if (this.search || this.selectedArtists.length !== 0 || this.selectedDrops.length !==  0) {
+      const artistIds = this.selectedArtists.map((x) => x.id).join(',');
+      const dropsIds = this.selectedDrops.map((x) => x.id).join(',');
+      const marketplaceInfoFetch = await fetch(`/api/marketplace/search?search=${this.search}&artist=${artistIds}&drop=${dropsIds}`);
+      set(this.model, 'marketplaceInfo',  await marketplaceInfoFetch.json());
     } else {
       const marketplaceInfoFetch = await fetch(`/api/marketplace`);
-      const marketplaceInfo = await marketplaceInfoFetch.json();
-      this.model = {marketplaceInfo: marketplaceInfo};
+      set(this.model, 'marketplaceInfo',  await marketplaceInfoFetch.json());
+
     }
-
-
   }
 
   @action
-  searchMarketplace(){
+  searchMarketplace() {
     debounce(this, this.searchMarketplaceFetch, 500);
   }
 
   @action
-  changeArtist(artist){
+  changeArtist(artist) {
     this.selectedArtists = artist
+    debounce(this, this.searchMarketplaceFetch, 500);
+  }
+
+  @action
+  changeDrop(drop) {
+    this.selectedDrops = drop
     debounce(this, this.searchMarketplaceFetch, 500);
   }
 
--- app/marketplace/secondary/route.js
+++ app/marketplace/secondary/route.js
@@ -5,8 +5,12 @@ export default class SecondaryRoute extends Route {
   @service abi;
 
   async model(params) {
-    const marketplaceInfoFetch = await fetch(`/api/marketplace`);
-    const marketplaceInfo = await marketplaceInfoFetch.json()
-    return {marketplaceInfo: marketplaceInfo};
+    const [marketplaceInfoFetch, facetsFetch] = await Promise.all([
+      fetch(`/api/marketplace`),
+      fetch(`/api/marketplace/facets`)
+    ]);
+    const marketplaceInfo = await marketplaceInfoFetch.json();
+    const facets = await facetsFetch.json();
+    return {marketplaceInfo: marketplaceInfo, facets: facets};
   }
 }
