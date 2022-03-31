f9912c467730f5c73545f66cd1bc689616afce6f
Adrien Vinches <ad.vinches@gmail.com>
2022-03-30 21:00:59 -0700

    add prices

---
+++ app/components/domains/prices.js
@@ -0,0 +1,6 @@
+import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+
+export default class DomainsPricesComponent extends Component {
+  @service intl;
+}
